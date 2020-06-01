6b9f19b19994b708161488c93c5d423167ed02c0
Alvee Noor Akand <alvee.akand@gmail.com>
2020-06-01 21:33:12 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,9 +1,9 @@
 # Write your solution here
 current_second = Time.now.to_i 
 
-is_even? = (current_second % 2 == 0)
+is_even = (current_second % 2 == 0)
 
-if is_even?
+if is_even
   puts "Even!"
 else
   puts"Odd!"
